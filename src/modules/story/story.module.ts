2e4391bc2a207077281b1c3a130c701f7ea63567
Nicholas Seiti Handa <nicholasseiti1206@gmail.com>
2023-04-22 10:13:20 -0300

    [dev] deleting unused import

--- src/modules/story/story.module.ts
+++ src/modules/story/story.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { StoryService } from './story.service';
 import { StoryController } from './story.controller';
 import { PrismaService } from 'src/database/prisma.service';
-import { StoryMapper } from 'src/mappers/story.mapper';
 
 @Module({
   controllers: [StoryController],
-  providers: [StoryService, PrismaService, StoryMapper],
+  providers: [StoryService, PrismaService],
 })
 export class StoryModule {}
