625ccded31678fb4883e5a526e6621c6c638421e
Nicholas Seiti Handa <nicholasseiti1206@gmail.com>
2023-04-29 15:13:54 -0300

    [feat] adding conditions to create story

--- src/modules/story/story.service.ts
+++ src/modules/story/story.service.ts
@@ -1,17 +1,73 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/database/prisma.service';
-import { StoryDTO } from './story.dto';
-import { StoryMapper } from 'src/mappers/story.mapper';
+import { PrismaService } from 'src/shared/database/prisma.service';
+import {
+  ComicMapper,
+  NovelMapper,
+  StoryMapper,
+  NovelChapterMapper,
+  ComicChapterMapper,
+} from 'src/common/mappers/index';
+import { isEmpty } from 'class-validator';
+import { StoryDTO } from 'src/modules/story/story.dto';
 
 @Injectable()
+//Crie um test case para os cenários de crud abaixo
 export class StoryService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: StoryDTO) {
     const storyEntity = StoryMapper.dtoToEntity(data);
+    const novelEntity = NovelMapper.dtoToEntity(data.novel);
+    const comicEntity = ComicMapper.dtoToEntity(data.comic);
+    const novelChaptersEntity = NovelChapterMapper.dtoListToEntity(
+      data.novel.chapters,
+    );
+    const comicChaptersEntity = ComicChapterMapper.dtoListToEntity(
+      data.comic.chapters,
+    );
+
+    //need test if could create a story when already exists a story with the same name or id
     const story = await this.prisma.story.create({
       data: storyEntity,
+      include: {
+        novel: { include: { chapters: true } },
+        comic: { include: { chapters: true } },
+      },
     });
+
+    if (story.id != null) {
+      if (!isEmpty(data.novel)) {
+        story.novel = await this.prisma.novel.create({
+          data: novelEntity,
+          include: { chapters: true },
+        });
+
+        if (story.novel.id != null) {
+          if (!isEmpty(data.novel.chapters)) {
+            novelChaptersEntity.forEach((novelChapterEntity) => {
+              this.prisma.novelChapter.create({
+                data: novelChapterEntity,
+              });
+            });
+          }
+        }
+      }
+
+      if (!isEmpty(data.comic)) {
+        story.comic = await this.prisma.comic.create({
+          data: comicEntity,
+          include: { chapters: true },
+        });
+
+        if (!isEmpty(data.novel.chapters)) {
+          comicChaptersEntity.forEach((comicChapterEntity) => {
+            this.prisma.comicChapter.create({
+              data: comicChapterEntity,
+            });
+          });
+        }
+      }
+    }
 
     return story;
   }
